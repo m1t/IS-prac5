fc1f529b7e955c5d76253c90f1bde504623f22cd
mits <mits.jp@gmail.com>
2013-12-08 17:20:55 +0900

    Initial Commit

---
+++ IS-prac5Tests/IS_prac5Tests.h
@@ -0,0 +1,13 @@
+//
+//  IS_prac5Tests.h
+//  IS-prac5Tests
+//
+//  Created by Chida Mitsuhiro on 13/12/08.
+//  Copyright (c) 2013年 Chida Mitsuhiro. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IS_prac5Tests : SenTestCase
+
+@end
---
+++ IS-prac5Tests/IS_prac5Tests.m
@@ -0,0 +1,32 @@
+//
+//  IS_prac5Tests.m
+//  IS-prac5Tests
+//
+//  Created by Chida Mitsuhiro on 13/12/08.
+//  Copyright (c) 2013年 Chida Mitsuhiro. All rights reserved.
+//
+
+#import "IS_prac5Tests.h"
+
+@implementation IS_prac5Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in IS-prac5Tests");
+}
+
+@end
